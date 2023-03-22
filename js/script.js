6e58ce88ae686802bfa8bf1f116e5837194b4bd9
Dima Stekhov <dimastekhov@gmail.com>
2023-03-22 20:39:55 +0200

    add burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let menu = document.querySelector('.menu');
+let menuBtn = document.querySelector('.menu__button');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	menu.classList.toggle('menu_active');
+	menuBtn.classList.toggle('menu__button_active');
+	document.body.classList.toggle('lock');
+}
+
+menu.addEventListener('click', menuScrollTo);
+
+function menuScrollTo(event) {
+	let menuLink = event.target.closest('.menu__link');
+	if (!menuLink) return;
+	toggleMenu();
+}
