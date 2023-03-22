0528b7378d89b4660e0a9e072953e14be86d41de
Dima Stekhov <dimastekhov@gmail.com>
2023-03-22 22:52:15 +0200

    fix menu close on resize change some menu styles

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@
 
 let menu = document.querySelector('.menu');
 let menuBtn = document.querySelector('.menu__button');
+let menuBurgerActive = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+menu.addEventListener('click', menuScrollTo);
+window.addEventListener('resize', removeMenuBurger);
+
+function removeMenuBurger() {
+	if (window.innerWidth > 768 && menuBurgerActive) {
+		menuBurgerActive = false;
+		menu.classList.remove('menu_active');
+		menuBtn.classList.remove('menu__button_active');
+		document.body.classList.remove('lock');
+	}
+}
 
 function toggleMenu() {
 	menu.classList.toggle('menu_active');
 	menuBtn.classList.toggle('menu__button_active');
 	document.body.classList.toggle('lock');
+	menuBurgerActive = !menuBurgerActive;
 }
-
-menu.addEventListener('click', menuScrollTo);
 
 function menuScrollTo(event) {
 	let menuLink = event.target.closest('.menu__link');
